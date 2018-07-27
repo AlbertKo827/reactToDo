4b4c3add5fffb1d0437127ddb5e434f8e26b8ef4
Myeong Seok Ko <myeongsku@gmail.com>
2018-07-27 23:59:31 +0900

    add components basic form

--- src/component/Appbar/top.tsx
+++ src/component/Appbar/top.tsx
@@ -4,8 +4,6 @@ import { WithStyles, withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 
 import styles from './style';
 
@@ -17,30 +15,12 @@ const Topbar : React.SFC<Iprops> = (props) => {
         <div>
             <AppBar position="static">
                 <Toolbar>
-                <Typography variant="title" color="inherit">
-                    Todo-List
-                </Typography>
-                <Button color="inherit">Login</Button>
+                    <Typography variant="title" color="inherit">
+                        To Do List
+                    </Typography>
                 </Toolbar>
             </AppBar>
-            <TextField
-        defaultValue="react-bootstrap"
-        label="Bootstrap"
-        id="bootstrap-input"
-        InputProps={{
-          disableUnderline: true,
-          classes: {
-            root: props.classes.bootstrapRoot,
-            input: props.classes.bootstrapInput,
-          },
-        }}
-        InputLabelProps={{
-          shrink: true,
-          className: props.classes.bootstrapFormLabel,
-        }}
-      />
         </div>
-        
     );
 }
 
