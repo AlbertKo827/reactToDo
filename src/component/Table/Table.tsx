58b5d101f0055cc55ec12cb531cb4e003cfa38d7
Myeong Seok Ko <myeongsku@gmail.com>
2018-07-30 12:55:40 +0900

    mapping data and binding

--- src/component/Table/Table.tsx
+++ src/component/Table/Table.tsx
@@ -5,30 +5,56 @@ import { WithStyles } from '@material-ui/core/styles'
 import styles from './style'
 import { Paper, Table, withStyles, TableBody, TableRow, TableCell, Checkbox } from '@material-ui/core';
 
+let count : number = 0;
+
+const createData  = (title : string, description : string) => {
+    count += 1;
+
+    return {id : count, title, description};
+}
+
 interface ITDTableProps extends WithStyles<typeof styles>{
 }
 
 interface ITDTableState{
-    id : number
+    data : Array<{
+        id : number,
+        title : string,
+        description : string
+    }>,
 }
 
 class TDTable extends React.Component<ITDTableProps, ITDTableState>{
+    constructor(props : ITDTableProps){
+        super(props);
+
+        this.state = {
+            data : [
+                createData('first', 'this is first item')
+            ]
+        }
+    }
+
     public render() {
         return (
             <Paper>
                 <Table>
                     <TableBody>
-                        <TableRow>
-                            <TableCell>
-                                <Checkbox />
-                            </TableCell>
-                            <TableCell>
-                                <div>
-                                    <p>Title</p>
-                                    <p>body</p>
-                                </div>
-                            </TableCell>
-                        </TableRow>
+                        {
+                            this.state.data
+                                .map(n => 
+                                    <TableRow key={n.id}>
+                                            <TableCell>
+                                                <Checkbox />
+                                            </TableCell>
+                                            <TableCell>
+                                                
+                                                <h4>{n.title}</h4>
+                                                <p>{n.description}</p>
+                                            </TableCell>
+                                    </TableRow>  
+                            )
+                        }
                     </TableBody>
                 </Table>
             </Paper>
